371a33c68221a95bba0a605a86af2a15090a05a8
johnsimonsen <johnsimonsen@weber.edu>
2020-02-21 14:38:19 -0500

    03 adn 04 complete

--- new_python_AKS_Tests/03-coin-flipper/test_03.py
+++ new_python_AKS_Tests/03-coin-flipper/test_03.py
@@ -1,1 +1,36 @@
-import coin_flipper as student
+import unittest, subprocess
+
+class Tests(unittest.TestCase):
+	file = "coin_flipper.py"
+	
+	def test_1(self):
+		inputs = "2\n\n"
+		correct = "How many times do you want to flip the coin?\n"
+		result = Tests.catchOutput(inputs)[:len(correct)]
+		self.assertEqual(result, correct)
+
+	def test_2(self):
+		inputs = "10\n1\n"
+		correct = "How many times do you want to flip the coin?\nPress enter to see the results.\n"
+		correct += "The coin was flipped 10 times.\nHeads: 5\tTails: 5\n"
+		result = Tests.catchOutput(inputs)
+		self.assertEqual(result, correct)
+	
+	def test_3(self):
+		inputs = "100\n0\n"
+		correct = "How many times do you want to flip the coin?\nPress enter to see the results.\n"
+		correct += "The coin was flipped 100 times.\nHeads: 50\tTails: 50\n"
+		result = Tests.catchOutput(inputs)
+		self.assertEqual(result, correct)
+	
+
+	# setup methods
+	@staticmethod
+	def catchOutput(inputs=None):
+		p = subprocess.run(["python3", Tests.file], capture_output=True, input=inputs, text=True)
+		if err:=p.stderr:
+			print(err)
+		return p.stdout
+
+if __name__ == '__main__':
+	unittest.main()
