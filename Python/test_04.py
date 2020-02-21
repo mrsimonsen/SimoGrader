fd681848a2be8f12afc614e2eb1ed93bad8df8e3
johnsimonsen <johnsimonsen@weber.edu>
2020-02-21 14:38:55 -0500

    temp files deleted

---
+++ Python/test_04.py
@@ -0,0 +1,70 @@
+import subprocess
+file = "GMN2.py"
+
+# setup methods
+def catchOutput(inputs=None):
+		p = subprocess.run(["python3", file], capture_output=True, input=inputs, text=True)
+		if err:=p.stderr:
+			print(err)
+		return p.stdout
+
+def main():
+	total = 0
+	score = 0
+	total += 1
+	#def test_1(self):
+	inputs = "0\n1\n1\n1\n1\n1\n1\n1\n"
+	correct = "\tWelcome to 'Guess My Number 2.0'!\n\nI'm thinking of a number between 1 and 100.\nYou have 6 attempts to guess my number.\nPress enter to begin.\n"
+	result = catchOutput(inputs)[:len(correct)]
+	if result == correct:
+		score += 1
+
+	total += 1
+	#def test_2(self):
+	inputs = "\n1\n1\n1\n1\n1\n1\n1\n"
+	correct = "\tWelcome to 'Guess My Number 2.0'!\n\nI'm thinking of a number between 1 and 100.\nYou have 6 attempts to guess my number.\nPress enter to begin.\nTake guess number 1:\n"
+	result = catchOutput(inputs)[:len(correct)]
+	if result == correct:
+		score += 1
+
+	total += 1
+	#def test_3(self):
+	inputs = "0\n47\n"
+	correct = "\tWelcome to 'Guess My Number 2.0'!\n\nI'm thinking of a number between 1 and 100.\nYou have 6 attempts to guess my number.\nPress enter to begin.\nTake guess number 1:\n"
+	correct += "You guessed it! The number was 47.\nAnd it only took you 1 tries!\n"
+	result = catchOutput(inputs)
+	if result == correct:
+		score += 1
+	
+	total += 1
+	#def test_4(self):
+	inputs = "1\n1\n1\n1\n1\n100\n1\n1\n"
+	correct = "\tWelcome to 'Guess My Number 2.0'!\n\nI'm thinking of a number between 1 and 100.\nYou have 6 attempts to guess my number.\nPress enter to begin.\nTake guess number 1:\n"
+	correct += "Higher...\nTake guess number 2:\n" 
+	correct += "Higher...\nTake guess number 3:\n"
+	correct += "Higher...\nTake guess number 4:\n"
+	correct += "Higher...\nTake guess number 5:\n"
+	correct += "Lower...\nTake guess number 6:\n"
+	correct += "You ran out of tries! The number was 62.\n"
+	result = catchOutput(inputs)
+	if result == correct:
+		score += 1
+		
+	#hidden tests
+	total += 1
+	def test_4(self):
+		inputs = "14\n50\n75\n90\n95\n93\n"
+		correct = "\tWelcome to 'Guess My Number 2.0'!\n\nI'm thinking of a number between 1 and 100.\nYou have 6 attempts to guess my number.\nPress enter to begin.\nTake guess number 1:\n"
+		correct += "Higher...\nTake guess number 2:\n" 
+		correct += "Higher...\nTake guess number 3:\n"
+		correct += "Higher...\nTake guess number 4:\n"
+		correct += "Lower...\nTake guess number 5:\n"
+		correct += "You guessed it! The number was 93.\nAnd it only took you 5 tries!\n"
+		result = catchOutput(inputs)
+	if result == correct:
+		score += 1
+	
+	return f"{score}/{total}"
+
+if __name__ == "__main__":
+	print(main())
