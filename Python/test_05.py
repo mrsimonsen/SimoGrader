96ebeba98e6f6a80377b68a12ddb9e3914816d5d
johnsimonsen <johnsimonsen@weber.edu>
2020-02-21 16:47:33 -0500

    05 complete

---
+++ Python/test_05.py
@@ -0,0 +1,54 @@
+import subprocess
+file = "dice_roller.py"
+
+# setup methods
+def catchOutput(inputs=None):
+		p = subprocess.run(["python3", file], capture_output=True, input=inputs, text=True)
+		if err:=p.stderr:
+			print(err)
+		return p.stdout
+
+def main():
+	total = 0
+	score = 0
+	total += 1
+	#def test_1(self):
+	inputs = "5\n10\n\n"
+	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nPress enter to roll 5x d10.\n"
+	result = catchOutput(inputs)[:len(correct)]
+	if result == correct:
+		score += 1
+
+	total += 1
+	#def test_2(self):
+	inputs = "3\n4\n0\n"
+	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nPress enter to roll 3x d4.\n"
+	correct += "Roll 1: 2\nRoll 2: 1\nRoll 3: 2\n"
+	result = catchOutput(inputs)[:len(correct)]
+	if result == correct:
+		score += 1
+
+	total += 1
+	#def test_3(self):
+	inputs = "3\n4\n1\n"
+	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nPress enter to roll 3x d4.\n"
+	correct += "Roll 1: 2\nRoll 2: 2\nRoll 3: 1\n"
+	correct += "Total: 5\n"
+	result = catchOutput(inputs)
+	if result == correct:
+		score += 1
+		
+	#hidden tests
+	total += 1
+	inputs = "5\n20\n14\n"
+	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nPress enter to roll 5x d20.\n"
+	correct += "Roll 1: 2\nRoll 2: 18\nRoll 3: 11\nRoll 4: 1\nRoll 5: 7\n"
+	correct += "Total: 39\n"
+	result = catchOutput(inputs)
+	if result == correct:
+		score += 1
+	
+	return f"{score}/{total}"
+
+if __name__ == "__main__":
+	print(main())
