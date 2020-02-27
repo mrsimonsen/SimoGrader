beee74400e5f3e3d56ff6d3ccb9f0221661531f4
johnsimonsen <johnsimonsen@weber.edu>
2020-02-26 21:55:33 -0500

    bookmark - stackoverflow answer in comment

--- new_python_AKS_Tests/11-character-creator/test_11.py
+++ new_python_AKS_Tests/11-character-creator/test_11.py
@@ -1,6 +1,7 @@
 import unittest
 from subprocess import run
 from os import getcwd
+import character_creator
 
 class Tests(unittest.TestCase):
 	file = "character_creator.py"
@@ -24,10 +25,30 @@ class Tests(unittest.TestCase):
 		inputs = "1\n0\n
 		correct = MENU
 		correct += "What is your choice?\n"
-		correct += '______________________________\nStrength\t|\t0\nDexterity\t|\t0\nConstitution\t|\t0\nWisdom\t\t|\t0\nIntelligence\t|\t0\nCharisma\t|\t0\nPool\t\t|\t72\n______________________________'
+		correct += ''' ______________________________
+Strength\t|\t0
+Dexterity\t|\t0
+Constitution\t|\t0
+Wisdom\t\t|\t0
+Intelligence\t|\t0
+Charisma\t|\t0
+Pool\t\t|\t72
+______________________________'''
 		result = Tests.catchOutput(inputs, '0')
 		self.assertEqual(result, correct)
-
+	
+	def test_3(self):
+		test_dict = {'Something': 0,
+					 'Another': 0,
+					 'Pool': 10}
+		#############################
+		#https://stackoverflow.com/questions/9829331/how-do-i-handle-multiple-asserts-within-a-single-python-unittest
+		##################
+		correct = "5 added to Something"
+		result = character_creator.add('Something', 5, test_dict)
+		self.assertEqual(result, correct)
+		correct = "15 is more points than you have left in Something"
+		
 # setup methods
 	@staticmethod
 	def catchOutput(inputs=None, seed=''):
