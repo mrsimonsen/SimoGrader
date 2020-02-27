76f0ca4505682072e3e674ceec138769c99a4baf
johnsimonsen <johnsimonsen@weber.edu>
2020-02-27 16:42:31 -0500

    bookmark, maybe different program than CC2 for more OOP

--- new_python_AKS_Tests/13-critter-caretaker-2.0/test_13.py
+++ new_python_AKS_Tests/13-critter-caretaker-2.0/test_13.py
@@ -1,1 +1,23 @@
-import CC2 as student
+import unittest
+from subprocess import run
+from os import getcwd
+
+class Tests(unittest.TestCase):
+	file = "CC2.py"
+
+	def test_1(self):
+		inputs = "a\n0\n"
+		correct = Tests.MENU
+		result = Tests.catchOutput(inputs)
+		self.assertEqual(result, correct)
+		
+		
+# setup methods
+	@staticmethod
+	def catchOutput(inputs=None, seed=''):
+		cwd = getcwd()
+		p = run(f"python3 {Tests.file} {seed}", capture_output=True, text=True, cwd=cwd, shell=True, input=inputs)
+		return p.stdout
+
+if __name__ == '__main__':
+	unittest.main()
