4c23c46a121341d0f72a09477a13ee35be310a84
johnsimonsen <johnsimonsen@weber.edu>
2020-02-27 10:12:58 -0500

    subTests!

--- new_python_AKS_Tests/11-character-creator/test_11.py
+++ new_python_AKS_Tests/11-character-creator/test_11.py
@@ -6,26 +6,27 @@ import character_creator
 class Tests(unittest.TestCase):
 	file = "character_creator.py"
 	MENU = '''
-\t\t ____
-\t\t|Menu|
-\t0 - Quit
-\t1 - View Attributes and Pool
-\t2 - Add to Attribute
-\t3 - Remove from Attribute\n'''
+\t\t\t ____
+\t\t\t|Menu|
+\t\t0 - Quit
+\t\t1 - View Attributes and Pool
+\t\t2 - Add to Attribute
+\t\t3 - Remove from Attribute\n'''
 
 	def test_1(self):
 		inputs = "0\n"
-		correct = MENU
-		correct += "What is your choice?\n"
+		correct = Tests.MENU
+		correct += "What's your choice?\n"
 		correct += "Goodbye.\n"
 		result = Tests.catchOutput(inputs)
 		self.assertEqual(result, correct)
 	
 	def test_2(self):
-		inputs = "1\n0\n
-		correct = MENU
-		correct += "What is your choice?\n"
-		correct += ''' ______________________________
+		inputs = "1\n0\n"
+		correct = Tests.MENU
+		correct += "What's your choice?\n"
+		correct += '''
+______________________________
 Strength\t|\t0
 Dexterity\t|\t0
 Constitution\t|\t0
@@ -33,21 +34,28 @@ Wisdom\t\t|\t0
 Intelligence\t|\t0
 Charisma\t|\t0
 Pool\t\t|\t72
-______________________________'''
+______________________________\n'''
+		correct += Tests.MENU
+		correct += "What's your choice?\n"
+		correct += "Goodbye.\n"
 		result = Tests.catchOutput(inputs, '0')
 		self.assertEqual(result, correct)
 	
 	def test_3(self):
 		test_dict = {'Something': 0,
-					 'Another': 0,
 					 'Pool': 10}
-		#############################
-		#https://stackoverflow.com/questions/9829331/how-do-i-handle-multiple-asserts-within-a-single-python-unittest
-		##################
-		correct = "5 added to Something"
-		result = character_creator.add('Something', 5, test_dict)
-		self.assertEqual(result, correct)
-		correct = "15 is more points than you have left in Something"
+		with self.subTest():
+			correct = "5 added to Something"
+			result = character_creator.add('Something', 5, test_dict)
+			self.assertEqual(result, correct)
+		with self.subTest():
+			correct = "6 is more points than you have left in your pool"
+			result = character_creator.add('Something', 6, test_dict)
+			self.assertEqual(result, correct)
+		with self.subTest():
+			correct = "'this' is not a valid attribute"
+			result = character_creator.add('this', 1, test_dict)
+			self.assertEqual(result, correct)
 		
 # setup methods
 	@staticmethod
