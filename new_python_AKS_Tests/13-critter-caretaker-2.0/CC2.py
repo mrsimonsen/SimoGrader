4eff010b3fcf814d8fa2ce4af09d527a7923437b
johnsimonsen <johnsimonsen@weber.edu>
2020-02-27 16:42:31 -0500

    bookmark, maybe different program than CC2 for more OOP

--- new_python_AKS_Tests/13-critter-caretaker-2.0/CC2.py
+++ new_python_AKS_Tests/13-critter-caretaker-2.0/CC2.py
@@ -54,46 +54,45 @@ class Critter(object):
         return "Wheee!"
 
 
-def main():
-    crit_name = input("What do you want to name your critter?: ")
-    crit = Critter(crit_name)
+crit_name = input("What do you want to name your critter?: ")
+crit = Critter(crit_name)
 
-    choice = None
-    while choice != "0":
-        print \
-        ("""
-        Critter Caretaker
+choice = None
+while choice != "0":
+	print \
+	("""
+	Critter Caretaker
 
-        0 - Quit
-        1 - Listen to your critter
-        2 - Feed your critter
-        3 - Play with your critter
-        """)
+	0 - Quit
+	1 - Listen to your critter
+	2 - Feed your critter
+	3 - Play with your critter
+	""")
 
-        choice = input("Choice: ")
-        print()
+	choice = input("Choice: ")
+	print()
 
-        # exit
-        if choice == "0":
-            print("Good-bye.")
+	# exit
+	if choice == "0":
+		print("Good-bye.")
 
-        # listen to your critter
-        elif choice == "1":
-            print(crit.talk())
+	# listen to your critter
+	elif choice == "1":
+		print(crit.talk())
 
-        # feed your critter
-        elif choice == "2":
-            food =int(input("How much food do you want to feed your critter? "))
-            print(crit.eat(food))
+	# feed your critter
+	elif choice == "2":
+		food =int(input("How much food do you want to feed your critter? "))
+		print(crit.eat(food))
 
-        # play with your critter
-        elif choice == "3":
-            fun =int(input("How much fun do you and your cirtter have? "))
-            print(crit.play(fun))
+	# play with your critter
+	elif choice == "3":
+		fun =int(input("How much fun do you and your cirtter have? "))
+		print(crit.play(fun))
 
-        # secret backdoor
-        elif choice == '14':
-            print(crit)
-        # some unknown choice
-        else:
-            print(f"\nSorry, but {choice} isn't a valid choice.")
+	# secret backdoor
+	elif choice == '14':
+		print(crit)
+	# some unknown choice
+	else:
+		print(f"\nSorry, but {choice} isn't a valid choice.")
