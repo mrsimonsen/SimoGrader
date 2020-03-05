5e50bb90000ce9de7a5d2394e81fb479cd502d66
johnsimonsen <johnsimonsen@weber.edu>
2020-03-05 15:52:27 -0500

    grader updated to use subprocess run instead of Popen

--- Java/SystemCommands.py
+++ Java/SystemCommands.py
@@ -1,31 +1,24 @@
 # https://stackoverflow.com/questions/7016249/compiling-java-from-python
+from subprocess import run
 
-import subprocess
-PIPE = subprocess.PIPE
 def compile_java(java_file):
 	'''compile java program, if output it failed'''
-	cmd = 'javac ' + java_file
-	proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-	out = proc.communicate()
-	if out[0] or out[1]:
+	p = run(f'javac {java_file}', shell=True, capture_output=True)
+	if p.stdout or p.stderr:
 		return False #compile failed - there was output
 	else:
 		return True
 
 def run_java(java_class):
 	'''run a compiled java program and caputre output, must be in the same dir as the file'''
-	cmd = "java " + java_class
-	proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-	out, err = proc.communicate()
-	if err:
-		print(err.decode())
-	return out.decode()
+	p = run(f'java {java_class}', shell=True, capture_output=True, text=True)
+	if e := p.stderr:
+		print(e)
+	return p.stdout
 
 def git_log():
 	'''get the timestamp of the latest commit'''
-	cmd = "git log -1 --format=%ci"
-	p = subprocess.Popen(cmd,shell=True,stdout=PIPE,stderr=PIPE)
-	out,err = p.communicate()
-	if err:
-		print(err.decode())
-	return out.decode()
+	p = run(f'git log -1 --format=%ci',shell=True,capture_output=True, text=True)
+	if e := p.stderr:
+		print(e)
+	return p.stdout
--- Java/grader.py
+++ Java/grader.py
@@ -1,8 +1,9 @@
 #java grader
-import os, csv, subprocess, datetime, shelve, sys
+import os, csv, datetime, shelve, sys
 import SystemCommands as sc
 from data_maker import Assignment,Student
 from data_maker import main as setup
+from subprocess import run
 
 def multi_run(assign_list):
 	if assign_list == None:
@@ -44,7 +45,7 @@ def intro():
 def gather(a):
 	root = os.getcwd()
 	#make sure old folder is deleted
-	subprocess.run(["rm", "-rf", "testing"])
+	run(["rm", "-rf", "testing"])
 	os.mkdir("testing")
 	data = shelve.open('grading_data')
 	students = data["students"]
@@ -52,11 +53,10 @@ def gather(a):
 		os.chdir('testing')
 		os.mkdir(s.github)
 		if a.folder == "20CaesarCipher":
-			subprocess.run(["cp", "..\\test.txt", f"{s.github}\\test.txt"])
+			run(["cp", "..\\test.txt", f"{s.github}\\test.txt"])
 		for i in a.file:
-			subprocess.run(["cp", os.path.join(root,s.github,a.folder,i), os.path.join(root,'testing',s.github,i)])
-
-			subprocess.run(["cp", os.path.join(root,a.test), os.path.join(root,'testing',s.github,a.test)])
+			run(["cp", os.path.join(root,s.github,a.folder,i), os.path.join(root,'testing',s.github,i)])
+			run(["cp", os.path.join(root,a.test), os.path.join(root,'testing',s.github,a.test)])
 			os.chdir('..')
 			os.chdir(s.github)
 			s.submit = format_date(sc.git_log())
@@ -107,7 +107,7 @@ def grade(a):
 	f.close()
 	os.chdir(root)
 	os.chdir('..')
-	subprocess.run(["cp", f"Repos/testing/{a.folder[:2]}report.csv", f"{a.folder[:2]}report.csv"])
+	run(["cp", f"Repos/testing/{a.folder[:2]}report.csv", f"{a.folder[:2]}report.csv"])
 	os.chdir(root)
 
 def string_to_math(thing):
