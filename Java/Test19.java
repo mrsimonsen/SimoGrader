41287c7cf8d4eec72154864842e2b8782da249c4
johnsimonsen <johnsimonsen@weber.edu>
2020-03-26 14:21:51 -0400

    test fix

--- Java/Test19.java
+++ Java/Test19.java
@@ -113,7 +113,31 @@ public class Test19 {
     total++;
     setInput("Number of Novels Authored\nAuthor name\nNumber of novels\nJane Austen,6\nCharles Dickens,20\nErnest Hemingway 9\nErnest hemingway9\nErnest Hemingway,9\nJack Kerouac,22\nF, Scott Fitzgerald,8\nF. Scott, Fitzgerald, 8\nF. Scott Fitzgerald,8\nMary Shelley,7\nCharlotte Bronte,5\nMark Twain,11\nAgatha Christie,seventythree\nAgatha Christie,seventythree\nAgatha Christie,seventythree\nAgatha Christie,73\nIan Flemming,14\nJ.K. Rowling,14\nStephen King,54\nOscar Wilde,1\n-1\n");
     student.main(args);
-    correct = "        Number of Novels Authored\n";
+    correct = "Enter a title for the data:\nYou entered: Number of Novels Authored\n\n";
+	correct += "Enter the column 1 header:\nYou entered: Author name\n\n";
+    correct += "Enter the column 2 header:\nYou entered: Number of novels\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Jane Austen\nData integer: 6\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Charles Dickens\nData integer: 20\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: No comma in string.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: No comma in string.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Ernest Hemingway\nData integer: 9\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Jack Kerouac\nData integer: 22\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: Too many commas in input.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: Too many commas in input.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: F. Scott Fitzgerald\nData integer: 8\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Mary Shelley\nData integer: 7\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Charlotte Bronte\nData integer: 5\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Mark Twain\nData integer: 11\n\n";
+    correct += "Enter a data point (-1 to stop input):\nError: Comma not followed by an integer.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: Comma not followed by an integer.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nError: Comma not followed by an integer.\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Agatha Christie\nData integer: 73\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Ian Flemming\nData integer: 14\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: J.K. Rowling\nData integer: 14\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Stephen King\nData integer: 54\n\n";
+	correct += "Enter a data point (-1 to stop input):\nData string: Oscar Wilde\nData integer: 1\n\n";
+	correct += "Enter a data point (-1 to stop input):\n\n";
+    correct += "        Number of Novels Authored\n";
     correct += "Author name         |       Number of novels\n";
     correct += "--------------------------------------------\n";
     correct += "Jane Austen         |                      6\n";
@@ -131,9 +155,9 @@ public class Test19 {
     correct += "Oscar Wilde         |                      1\n";
     result = getOutput();
 	//cut length of entering data
-  	result = result.substring(1832, result.length());
+  	//result = result.substring(1832, result.length());
 	//cut off end
-	result = result.substring(0, 709);
+	result = result.substring(0, correct.length());
     restoreSystem();
     if (result.equals(correct)){
       score++;
