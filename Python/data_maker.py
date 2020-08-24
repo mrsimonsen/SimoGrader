93179a3b7f0da47ac6e9d8158ed9b3f1710dccf6
John Simonsen <johnsimonsen@weber.edu>
2020-08-24 12:10:15 -0600

    actual student import

--- Python/data_maker.py
+++ Python/data_maker.py
@@ -3,108 +3,112 @@ import csv, shelve, datetime
 #support classes
 class Assignment(object):
 	'''an assignment with a file name, due date, and testing file'''
-	
+
 	def __init__(self, file, due, test):
 		self.file = file
 		self.due = due
 		self.test = test
-		
-		def __str__(self):
-			rep = f"{self.file}\n{self.due}\n{self.test}"
-			return rep
-			
-			class Student(object):
-				'''a student with name, class period, and github username'''
-				
-				def __init__(self, name, period, github):
-					self.name = name
-					self.period = period
-					self.github = github
-					self.assignment = Assignment('error',datetime.datetime.today(),'') #default assignment object
-					self.score = 0 #default score
-					self.late = True #default late status
-					self.submit = None #default submisison time
-					
-					def __str__(self):
-						rep = f"{self.name}, Period {self.period}\n{self.github}\n--Current Assignment--\n{self.assignment.file}\n{self.score} points\nSubmitted:{self.submit}\nLate = {self.late}"
-						return rep
-						
-						def set_grade(self, assign_obj, score):
-							self.assignment = assign_obj
-							self.score = score
-							if assign_obj.due > self.submit:
-								self.late = False
-								def main():
-									#create shelve file, overwrite old file if exists
-									data = shelve.open('grading_data','n')
-									
-									#assignment details
-									assignments = ('00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15')
-									file_names = {'00':'hello_world.py',
-									'01':'calculator.py',
-									'02':'fortune_cookie.py',
-									'03':'coin_flipper.py',
-									'04':'GMN2.py',
-									'05':'dice_roller.py',
-									'06':'counter.py',
-									'07':'reverse_message.py',
-									'08':'right_triangle.py',
-									'09':'WJ2.py',
-									'10':'scrambler.py',
-									'11':'guess_AI.py',
-									'12':'character_creator.py',
-									'13':'CC2.py',
-									'14':'tv-remote.py',
-									'15':'pig-latin.py',
-									}
-									due_dates = {'00':datetime.datetime(2022,8,30,23,59,59),
-									'01':datetime.datetime(2020,9,6,23,59,59),
-									'02':datetime.datetime(2020,9,13,23,59,59),
-									'03':datetime.datetime(2020,9,13,23,59,59),
-									'04':datetime.datetime(2020,9,20,23,59,59),
-									'05':datetime.datetime(2020,9,27,23,59,59),
-									'06':datetime.datetime(2020,9,27,23,59,59),
-									'07':datetime.datetime(2020,10,4,23,59,59),
-									'08':datetime.datetime(2020,10,4,23,59,59),
-									'09':datetime.datetime(2020,10,11,23,59,59),
-									'10':datetime.datetime(2020,10,25,23,59,59),
-									'11':datetime.datetime(2020,11,1,23,59,59),
-									'12':datetime.datetime(2020,11,8,23,59,59),
-									'13':datetime.datetime(2020,11,15,23,59,59),
-									'14':datetime.datetime(2020,11,29,23,59,59),
-									'15':datetime.datetime(2020,12,13,23,59,59),
-									}
-									tests = {'00': 'test_00.py',
-									'01': 'test_01.py',
-									'02': 'test_02.py',
-									'03': 'test_03.py',
-									'04': 'test_04.py',
-									'05': 'test_05.py',
-									'06': 'test_06.py',
-									'07': 'test_07.py',
-									'08': 'test_08.py',
-									'09': 'test_09.py',
-									'10': 'test_10.py',
-									'11': 'test_11.py',
-									'12': 'test_12.py',
-									'13': 'test_13.py',
-									'14': 'test_14.py',
-									'15': 'test_15.py',
-									}
-									for i in assignments:
-										data[i]=Assignment(file_names[i],due_dates[i],tests[i])
-										
-										#student details
-										names = {}
-										students = []
-										with open("What's in a Username_ (Responses) - Form Responses 1.csv",'r',newline='') as f:
-											#format = time,first,last,period,git
-											raw = csv.reader(f,delimiter=',',quotechar='"')
-											for row in raw:
-												if row[0] == "Timestamp":
-													continue#skip the first row/header
-													students.append(Student(f"{row[2]}, {row[1]}",row[4],row[3]))
-													data['students'] = students
-													
-													#save all the things
-													data.close()
+
+	def __str__(self):
+		rep = f"{self.file}\n{self.due}\n{self.test}"
+		return rep
+
+class Student(object):
+	'''a student with name, class period, and github username'''
+
+	def __init__(self, name, period, github):
+		self.name = name
+		self.period = period
+		self.github = github
+		self.assignment = Assignment('error',datetime.datetime.today(),'') #default assignment object
+		self.score = 0 #default score
+		self.late = True #default late status
+		self.submit = None #default submisison time
+
+	def __str__(self):
+		rep = f"{self.name}, Period {self.period}\n{self.github}\n--Current Assignment--\n{self.assignment.file}\n{self.score} points\nSubmitted:{self.submit}\nLate = {self.late}"
+		return rep
+
+	def set_grade(self, assign_obj, score):
+		self.assignment = assign_obj
+		self.score = score
+		if assign_obj.due > self.submit:
+			self.late = False
+
+def main():
+	#create shelve file, overwrite old file if exists
+	data = shelve.open('grading_data','n')
+
+	#assignment details
+	assignments = ('00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','1')
+	file_names = {'00':'hello_world.py',
+	'01':'calculator.py',
+	'02':'fortune_cookie.py',
+	'03':'coin_flipper.py',
+	'04':'GMN2.py',
+	'05':'dice_roller.py',
+	'06':'counter.py',
+	'07':'reverse_message.py',
+	'08':'right_triangle.py',
+	'09':'WJ2.py',
+	'10':'scrambler.py',
+	'11':'guess_AI.py',
+	'12':'character_creator.py',
+	'13':'CC2.py',
+	'14':'tv-remote.py',
+	'15':'pig-latin.py',
+	'1':'hi.py'
+	}
+	due_dates = {'00':datetime.datetime(2022,8,30,23,59,59),
+	'01':datetime.datetime(2020,9,6,23,59,59),
+	'02':datetime.datetime(2020,9,13,23,59,59),
+	'03':datetime.datetime(2020,9,13,23,59,59),
+	'04':datetime.datetime(2020,9,20,23,59,59),
+	'05':datetime.datetime(2020,9,27,23,59,59),
+	'06':datetime.datetime(2020,9,27,23,59,59),
+	'07':datetime.datetime(2020,10,4,23,59,59),
+	'08':datetime.datetime(2020,10,4,23,59,59),
+	'09':datetime.datetime(2020,10,11,23,59,59),
+	'10':datetime.datetime(2020,10,25,23,59,59),
+	'11':datetime.datetime(2020,11,1,23,59,59),
+	'12':datetime.datetime(2020,11,8,23,59,59),
+	'13':datetime.datetime(2020,11,15,23,59,59),
+	'14':datetime.datetime(2020,11,29,23,59,59),
+	'15':datetime.datetime(2020,12,13,23,59,59),
+	'1':datetime.datetime.today()
+	}
+	tests = {'00': 'test_00.py',
+	'01': 'test_01.py',
+	'02': 'test_02.py',
+	'03': 'test_03.py',
+	'04': 'test_04.py',
+	'05': 'test_05.py',
+	'06': 'test_06.py',
+	'07': 'test_07.py',
+	'08': 'test_08.py',
+	'09': 'test_09.py',
+	'10': 'test_10.py',
+	'11': 'test_11.py',
+	'12': 'test_12.py',
+	'13': 'test_13.py',
+	'14': 'test_14.py',
+	'15': 'test_15.py',
+	'1':'test_hi.py'
+	}
+	for i in assignments:
+		data[i]=Assignment(file_names[i],due_dates[i],tests[i])
+
+	#student details
+	names = {}
+	students = []
+	with open("What's in a Username_ (Responses) - Copy of Form Responses 1.csv",'r',newline='') as f:
+		#format = time,first,last,period,git
+		raw = csv.reader(f,delimiter=',',quotechar='"')
+		for row in raw:
+			if row[0] == "Timestamp":
+				continue#skip the first row/header
+				students.append(Student(f"{row[2]}, {row[1]}",row[3],row[4]))
+				data['students'] = students
+
+	#save all the things
+	data.close()
--- Python/grader.py
+++ Python/grader.py
@@ -9,7 +9,7 @@ def multi_run(assign_list):
 		assigns = sys.argv[1:]
 		assign_list = []
 		setup()
-		data = sheleve.open('grading_data')
+		data = shelve.open('grading_data')
 		for a in assigns:
 			assign_list.append(data[a])
 		return assign_list
@@ -25,95 +25,98 @@ def git_log():
 	if e := p.stderr:
 		print(e)
 	return p.stdout
-	
+
 def intro():
-    setup()
-    n = True
+	setup()
+	n = True
 	if len(sys.argv)-1:
 		assign = sys.argv[1]
 		data = shelve.open('grading_data')
 		assign_obj = data[assign]
 		n = False
 	while n:
-    	print("Python Grader")
-    	print("This program needs to be ran from the parent directory of the collection of student repos")
-    	print()
-        assign = input("What is the number of the assignment folder?\n")
-        try:
-            data = shelve.open('grading_data')
-            assign_obj = data[assign]
-            n = False
-        except:
-            print("That wasn't a valid assignment number!")
-        data.close()
-    return assign_obj
+		print("Python Grader")
+		print("This program needs to be ran from the parent directory of the collection of student repos")
+		print()
+		assign = input("What is the number of the assignment?\n")
+		try:
+			data = shelve.open('grading_data')
+			assign_obj = data[assign]
+			n = False
+		except:
+			print("That wasn't a valid assignment number!")
+		data.close()
+	return assign_obj
 
 def gather(a):
-    root = os.getcwd()
+	root = os.getcwd()
 	#make sure old folder is deleted
-    run(['rm', '-rf', 'testing'])
-    os.mkdir("testing")
-    data = shelve.open('grading_data')
-    students = data['students']
-    for s in students:
+	try:
+		run(['rm', '-rf', 'testing'])
+	except:
+		print("old testing folder deleted")
+	os.mkdir("testing")
+	data = shelve.open('grading_data')
+	students = data['students']
+	for s in students:
 		os.chdir('testing')
 		os.mkdir(s.github)
-        run(['cp',  os.path.join(root,s.github,a.folder,a.file), os.path.join(root,'testing',s.github,a.file)])
-        run(['cp', os.path.join(root,a.test), os.path.join(root,'testing',s.github,'Test.py')])
+		run(['cp',  os.path.join(root,s.github,a.file), os.path.join(root,'testing',s.github,a.file)])
+		run(['cp', os.path.join(root,a.test), os.path.join(root,'testing',s.github,'Test.py')])
 		os.chdir('..')
-        os.chdir(s.github)
+		os.chdir(s.github)
 		s.submit = format_date(git_log())
 		os.chdir(root)
 	data['students']=students
-    data.close()
+	data.close()
 
 def format_date(raw):
-    #format '2019-08-28 14:46:11 -0600'
-    #index:  0123456789012345678901234
-    year = int(raw[:4])
-    month = int(raw[5:7])
-    day = int(raw[8:10])
-    hour = int(raw[11:13])
-    minute = int(raw[14:16])
-    second = int(raw[17:19])
-    date = datetime.datetime(year, month, day, hour, minute, second)
-    return date
+	#format '2019-08-28 14:46:11 -0600'
+	#index:  0123456789012345678901234
+	year = int(raw[:4])
+	month = int(raw[5:7])
+	day = int(raw[8:10])
+	hour = int(raw[11:13])
+	minute = int(raw[14:16])
+	second = int(raw[17:19])
+	date = datetime.datetime(year, month, day, hour, minute, second)
+	return date
 
 def grade(a):
-    data = shelve.open('grading_data')
-    s = data['students']
+	data = shelve.open('grading_data')
+	s = data['students']
 	data.close()
-    root = os.getcwd()
-    os.chdir('testing')
-    with open(f'{a.folder[:02]}report.csv','w',newline='') as f:
-        w = csv.writer(f,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        w.writerow(['Period','Student Name','Assignment Name','Points Earned','Is Late?'])
-    folders = [f.name for f in os.scandir() if f.is_dir()]
-    for f in folders:
-        print(f"Grading: {f}")
-        os.chdir(f)
-        p = run("py test.py", shell=True, capture_output=True, text=True)
-        if e := p.stderr:
+	root = os.getcwd()
+	os.chdir('testing')
+	with open(f'{a.test[-5:-3]}report.csv','w',newline='') as f:
+		w = csv.writer(f,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		w.writerow(['Period','Student Name','Assignment Name','Points Earned','Is Late?'])
+	folders = [f.name for f in os.scandir() if f.is_dir()]
+	for f in folders:
+		print(f"Grading: {f}")
+		os.chdir(f)
+		p = run("py test.py", shell=True, capture_output=True, text=True)
+		if e := p.stderr:
 			#had an error - auto fail
-            print(e)
-            points = 0
-        else:
+			print(e)
+			points = 0
+		else:
 			score = p.stdout
-            points = string_to_math(score)
+			points = string_to_math(score)
 		os.chdir('..')
-        for student in s:
-            if student.github == f:
-                student.set_grade(a, points)
-    f = open(f'{a.folder[:2]}report.csv','a',newline='')
-    w = csv.writer(f,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    s.sort(key=lambda x: x.name)
-    for i in s:
-        w.writerow([i.period,i.name,i.assignment.folder,i.score,i.late])
-    f.close()
-    os.chdir(root)
+		for student in s:
+			if student.github == f:
+				student.set_grade(a, points)
+	f = open(f'{a.test[-5:-3]}report.csv','a',newline='')
+	w = csv.writer(f,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	s.sort(key=lambda x: x.name)
+	for i in s:
+		w.writerow([i.period,i.name,i.assignment.file[-5:-3],i.score,i.late])
+		f.close()
+		os.chdir(root)
 	os.chdir('..')
-	run(['cp', f"Repos/Testing/{a.folder[:2]}report.csv", f"{a.folder[:2]}report.csv"])
-    os.chdir(root)
+	run(['cp', f"Repos/Testing/{a.file[:2]}report.csv", f"{a.file[:2]}report.csv"])
+	os.chdir(root)
 
 def string_to_math(thing):
 	x = thing.split("/")
@@ -123,15 +126,15 @@ def string_to_math(thing):
 		return round(score/total * 10,2)
 	except:
 		return 0
-    
+
 def main():
-    assign_list = None
-	if len(sys.argv)-2:
+	assign_list = None
+	if len(sys.argv) < 1:
 		assign_list = multi_run(assign_list)
 		while assign_list:
 			assign_obj = assign_list.pop(0)
 			print(assign_obj)
-			print(f"--Gathering Files {assign_obj.folder}--")	
+			print(f"--Gathering Files {assign_obj.folder}--")
 			gather(assign_obj)
 			print(f'--Starting Grading--')
 			grade(assign_obj)
@@ -146,4 +149,4 @@ def main():
 	print("--Testing Complete--")
 
 if __name__ == '__main__':
-    main()
+	main()
