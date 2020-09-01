b9c91d632d741a83074808ac7e7e60c239771e6b
John Simonsen <johnsimonsen@weber.edu>
2020-09-01 14:49:42 -0600

    Update test_05.py

--- Python/test_05.py
+++ Python/test_05.py
@@ -24,7 +24,7 @@ def main():
 	#def test_2(self):
 	inputs = "3\n4\n"
 	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nRolling 3x d4.\n"
-	correct += "Roll 1: 2\nRoll 2: 1\nRoll 3: 2\n"
+	correct += "Roll 1: 4\nRoll 2: 4\nRoll 3: 1\n"
 	result = catchOutput(inputs, '0')[:len(correct)]
 	if result == correct:
 		score += 1
@@ -33,7 +33,7 @@ def main():
 	#def test_3(self):
 	inputs = "3\n4\n"
 	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nRolling 3x d4.\n"
-	correct += "Roll 1: 2\nRoll 2: 2\nRoll 3: 1\n"
+	correct += "Roll 1: 2\nRoll 2: 1\nRoll 3: 3\n"
 	correct += "Total: 5\n"
 	result = catchOutput(inputs, '1')
 	if result == correct:
@@ -43,8 +43,8 @@ def main():
 	total += 1
 	inputs = "5\n20\n"
 	correct = "How many dice would you like to roll?\nHow many sides do the dice have?\nRolling 5x d20.\n"
-	correct += "Roll 1: 2\nRoll 2: 18\nRoll 3: 11\nRoll 4: 1\nRoll 5: 7\n"
-	correct += "Total: 39\n"
+	correct += "Roll 1: 4\nRoll 2: 20\nRoll 3: 17\nRoll 4: 8\nRoll 5: 9\n"
+	correct += "Total: 58\n"
 	result = catchOutput(inputs, '14')
 	if result == correct:
 		score += 1
