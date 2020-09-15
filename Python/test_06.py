aa101a9b0851de68b3393f3200823eabc51f003e
mrsimonsen <johnsimonsen@weber.edu>
2020-09-15 15:30:24 -0600

    testing fix

--- Python/test_06.py
+++ Python/test_06.py
@@ -5,7 +5,7 @@ file = "counter.py"
 # setup methods
 def catchOutput(inputs=None, seed=''):
 	cwd = getcwd()
-	p = run(f"python3 {Tests.file} {seed}", capture_output=True, text=True, cwd=cwd, shell=True, input=inputs)
+	p = run(f"python3 {file} {seed}", capture_output=True, text=True, cwd=cwd, shell=True, input=inputs)
 	return p.stdout
 
 def main():
