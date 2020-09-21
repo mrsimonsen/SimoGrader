32f6a72f5a17cccb21ed9fae65bc5e47080be960
mrsimonsen <johnsimonsen@weber.edu>
2020-09-21 10:40:54 -0600

    multi-grading

--- all.sh
+++ all.sh
@@ -1,10 +1,6 @@
-mkdir testing
-python3 simogit.py
-cp all.py testing/.
-mv Repos/ testing/.
-cd testing/
-cp Repos/* .
-clear
-python3 all.py
+cp assignment.txt Repos/.
+cd Repos
+python3 grader.py
+rm -rf testing
 cd ..
-rm -rf testing/
+
