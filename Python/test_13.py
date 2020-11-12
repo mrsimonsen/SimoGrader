133642537a58deb0739a39e230d1255dcdbe5e83
mrsimonsen <=>
2020-11-12 09:36:17 -0700

    text update

--- Python/test_13.py
+++ Python/test_13.py
@@ -47,20 +47,84 @@ def main():
 	correct += "How much food do you want to feed your critter?\n"
 	correct += "Brruppp. Thank you.\n"
 	correct += MENU + "Choice:\n"
-	correct += "How much fun do you and your cirtter have?\n"
+	correct += "How much fun do you and your critter have?\n"
 	correct += "Wheee!\n"
 	correct += MENU + "Choice:\n"
 	correct += "Critter Object\nName: Bob\nHunger: 7\nBoredom: 1\nMood: okay\n"
 	correct += MENU + "Choice:\n"
 	correct += "Goodbye.\n"
+	correct2 = "What do you want to name your critter?\n"
+	correct2 += MENU + "Choice:\n"
+	correct2 += "How much food do you want to feed your critter?\n"
+	correct2 += "Brruppp. Thank you.\n"
+	correct2 += MENU + "Choice:\n"
+	correct2 += "How much fun do you and your cirtter have?\n"
+	correct2 += "Wheee!\n"
+	correct2 += MENU + "Choice:\n"
+	correct2 += "Critter Object\nName: Bob\nHunger: 7\nBoredom: 1\nMood: okay\n"
+	correct2 += MENU + "Choice:\n"
+	correct2 += "Goodbye.\n"
 	result = catchOutput(inputs, '0')
-	if result == correct:
+	if result == correct or result == correct2:
 		score += 1
 	
 	#hidden tests
 	total += 1
 	inputs = "Dog\n1\n2\n10\n3\n10\n14\n0\n"
 	correct = '''What do you want to name your critter?
+
+Critter Caretaker
+
+0 - Quit
+1 - Listen to your critter
+2 - Feed your critter
+3 - Play with your critter
+Choice:
+I'm Dog and I feel okay now.
+
+Critter Caretaker
+
+0 - Quit
+1 - Listen to your critter
+2 - Feed your critter
+3 - Play with your critter
+Choice:
+How much food do you want to feed your critter?
+Brruppp. Thank you.
+
+Critter Caretaker
+
+0 - Quit
+1 - Listen to your critter
+2 - Feed your critter
+3 - Play with your critter
+Choice:
+How much fun do you and your critter have?
+Wheee!
+
+Critter Caretaker
+
+0 - Quit
+1 - Listen to your critter
+2 - Feed your critter
+3 - Play with your critter
+Choice:
+Critter Object
+Name: Dog
+Hunger: 2
+Boredom: 1
+Mood: happy
+
+Critter Caretaker
+
+0 - Quit
+1 - Listen to your critter
+2 - Feed your critter
+3 - Play with your critter
+Choice:
+Goodbye.
+'''
+	correct2 = '''What do you want to name your critter?
 
 Critter Caretaker
 
@@ -114,7 +178,7 @@ Choice:
 Goodbye.
 '''
 	result = catchOutput(inputs, 4)
-	if result == correct:
+	if result == correct or result == correct2:
 		score += 1
 	
 	return f"{score}/{total}"
