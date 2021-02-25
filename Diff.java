a99b6bae5d49af6d29881dfe96e757277a4303a0
mrsimonsen <johnsimonsen@weber.edu>
2021-02-25 13:50:22 -0700

    diff tool with color done

--- Diff.java
+++ Diff.java
@@ -6,8 +6,8 @@ public class Diff{
 		int last = 0;
 		for (int i=0; i<thing.length();i++){
 			if(thing.charAt(i) == '\n'){
-				list.add(thing.substring(last, i));
-				last = i;
+				list.add(thing.substring(last, i)+"\n");
+				last = i+1;
 			}
 		}
 		return list;
@@ -28,26 +28,9 @@ public class Diff{
 		return notFound;
 	}
 
-	public static String getChar(char thing){
-		//display escape sequence if it's a whitespace character
-		if (Character.isWhitespace(thing)){//check the correct character
-			switch (thing){
-				case '\t':
-					return "\\t"; 
-				case '\n':
-					return "\\n";
-				case '\r':
-					return "\\r";
-			}
-		}
-		else{
-			return ""+thing;
-		}
-	}
-
 	public static String mark(String rLine, int index){
 			//return a carrot pointing to the different character in the line
-			String mark = "";
+			String mark = " ";
 			for(int i=0; i<rLine.length(); i++){
 				if(i == index){
 					mark += "^";
@@ -59,6 +42,78 @@ public class Diff{
 			return mark;
 		}
 
+	public static String convertChar(char current){
+		String converted = "";
+		if(Character.isWhitespace(current)){
+			switch(current){
+				case '\t':
+					converted += "\\t";
+					break;
+				case '\n':
+					converted += "\\n";
+					break;
+				case '\r':
+					converted += "\\r";
+					break;
+				default:
+					converted += current;
+			}
+		}
+		else{
+			converted += current;
+		}
+		return converted;
+	}
+
+	public static void convertLines(ArrayList<String> lines){
+		String converted;
+		String line;
+		for(int i = 0;i<lines.size();i++){
+			converted = "";
+			line = lines.get(i);
+			for(int j = 0; j<line.length();j++){
+				converted += convertChar(line.charAt(j));
+			}
+			lines.set(i,converted);
+		}
+	}
+
+	public static String getDiff(ArrayList<String> lines, int line, int index){
+		String diff = "";
+		diff += lines.get(line).charAt(index);
+		if (diff.equals("\\")){
+			diff += lines.get(line).charAt(index+1);
+		}
+		return diff;
+	}
+
+	public static String color(String line, int index, String rdiff){
+	//text color
+	String RESET = "\u001B[0m";
+	String BLACK = "\u001B[30m";
+	String RED = "\u001B[31m";
+	String GREEN = "\u001B[32m";
+	String YELLOW = "\u001B[33m";
+	String BLUE = "\u001B[34m";
+	String PURPLE = "\u001B[35m";
+	String CYAN = "\u001B[36m";
+	String WHITE = "\u001B[37m";
+	//background color
+	String BLACK_BACKGROUND = "\u001B[40m";
+	String RED_BACKGROUND = "\u001B[41m";
+	String GREEN_BACKGROUND = "\u001B[42m";
+	String YELLOW_BACKGROUND = "\u001B[43m";
+	String BLUE_BACKGROUND = "\u001B[44m";
+	String PURPLE_BACKGROUND = "\u001B[45m";
+	String CYAN_BACKGROUND = "\u001B[46m";
+	String WHITE_BACKGROUND = "\u001B[47m";
+
+	String highlight = BLUE_BACKGROUND+RED+line.substring(0,index);
+	highlight += BLACK_BACKGROUND+WHITE + rdiff + BLUE_BACKGROUND+RED;
+	highlight += line.substring(index + rdiff.length(),line.length())+RESET;
+	return highlight;
+	}
+
 	public static String difference(String result, String correct) {
 		int line;//line number
 		int index;//index of different character in the line
@@ -66,10 +121,14 @@ public class Diff{
 		String cdiff = "";//what the character was supposed to be
 		String rdiff = "";//what the character was
 
-		//split both strings into arrays at \n
+		//split both strings into arrays at \n but keep the newline
 		ArrayList<String> rList = splitter(result);
 		ArrayList<String> cList = splitter(correct);
 
+		//convert lines to use escape characters for whitespace
+		convertLines(rList);
+		convertLines(cList);
+	
 		//find the line and index number of the first difference
 		int[] temp = locate(rList,cList);
 		line = temp[0];
@@ -78,37 +137,37 @@ public class Diff{
 		//if the strings were different
 		if (index >= 0 && line >= 0) {
 			//get the different characters
-			cdiff = getChar(cList[line].charAt(index));
-			rchar = getChar(rList[line].charAt(index));
-				
-			String[] context = {"","","",""};
-			if(line > 0){
-				context[0] ="Line "+(line-1)+":" +"\t"+ rList[line-1];
-			}
-			context[1] = "Line "+(line)+":"+"\t"+rList[line];
-			context[2] = "       "+mark(rLine[line],index);
-			if(line < (rList.length-1)){
-				context[3] = "Line "+(line+1)+":"+"\t"+rList[line+1];
-			}
-
-			//TODO: Add color - https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+			cdiff = getDiff(cList, line, index);
+			rdiff = getDiff(rList, line, index);
+			
+			//Add color to affected line- https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+			String highlight = color(rList.get(line),index,rdiff);
 			//make the message
 			message += "Expected '"+cdiff+"', but was '"+rdiff+"'\n";
 			message += "On line "+line+", index "+index+"\n";
 			message += "Context:\n";
+			String[] context = {"","","",""};
+			if(line > 0){
+				context[0] ="Line "+(line-1)+": "+ rList.get(line-1);
+			}
+			context[1] = "Line "+(line)+": "+highlight;
+			context[2] = "       "+mark(rList.get(line),index);
+			if(line < (rList.size()-1)){
+				context[3] = "Line "+(line+1)+": "+rList.get(line+1);
+			}
 			for(String in: context){
 				message += in+"\n";
 			}
 		}
 		else{
 			message = "";
-		*/
+		}
 		return message;
 	}
 
 	public static void main(String[] args){
 		String correct = "line 1\nline 2\nline 3\nline 4\nHello World!\nanother\nline";
-		String result = "line 1\nline 2\nline 3\nline 4 Hello WOrld\nanother\nline";
+		String result = "line 1\nline 2\nline 3\nline 4\nHello WOrld\nanother\nline";
 		System.out.println(difference(result, correct));
 	}
 }
