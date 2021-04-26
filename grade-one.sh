40c7d81ff12178e87081d402ec4b2a789644ddda
mrsimonsen <johnsimonsen@weber.edu>
2021-04-26 14:33:13 -0600

    clean up

--- grade-one.sh
+++
@@ -1,8 +0,0 @@
-read -p "assignment code: " code
-read -p "GitHub username: " username
-export name=$code-$username
-gh repo clone NUAMES-CS/$name
-export code=${code:0:2}
-cp ../AutoGrader/Python/test_$code.py $name/.
-python3 $name/test_$code.py
-rm -rf $name
