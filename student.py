d7ec76f200ab02ce0c912e985cacddeeda907ae9
mrsimonsen <johnsimonsen@weber.edu>
2021-04-27 15:56:46 -0600

    new make_data complete

---
+++ student.py
@@ -0,0 +1,28 @@
+class Student():
+	def __init__(self, name = "Student, Sample", period = 0, github = 'username')
+		self.name = name
+		self.period = period
+		self.github = github
+		self.assignments = []
+		self.submissions = 0
+	
+	def __str__(self):
+		rep = f"{self.period}: {self.name}\n"
+		rep += f"{self.class} - {self.github}\n"
+		rep += f"{Submissions: {self.submissions}"
+		return rep
+
+	@property
+	def class(self):
+		if self.period in classes['1400']:
+			return "Java"
+		elif self.period in in Student.classes['1030']:
+			return "Python"
+	
+	def add_assignments(self):
+		for i in 
+		self.assignments.append(assign)
+
+	
+
+		
