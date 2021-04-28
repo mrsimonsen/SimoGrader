f60858d0c195227cae08881fcae47ded7828d469
mrsimonsen <johnsimonsen@weber.edu>
2021-04-27 18:05:15 -0600

    student.py complete

--- student.py
+++ student.py
@@ -1,28 +1,35 @@
+import shelve
+
 class Student():
-	def __init__(self, name = "Student, Sample", period = 0, github = 'username')
+	def __init__(self, name = "Student, Sample", period = 0, github = 'username'):
 		self.name = name
 		self.period = period
 		self.github = github
 		self.assignments = []
-		self.submissions = 0
 	
 	def __str__(self):
-		rep = f"{self.period}: {self.name}\n"
-		rep += f"{self.class} - {self.github}\n"
-		rep += f"{Submissions: {self.submissions}"
+		rep = f"{self.name}: {self.github}\n"
+		d = shelve.open('data.dat')
+		course = d['periods'][self.period-1]
+		d.close()
+		rep += f"{self.period} - {course}\n"
 		return rep
-
-	@property
-	def class(self):
-		if self.period in classes['1400']:
-			return "Java"
-		elif self.period in in Student.classes['1030']:
-			return "Python"
 	
 	def add_assignments(self):
-		for i in 
-		self.assignments.append(assign)
+		d = shelve.open('data.dat')
+		course = d['periods'][self.period-1]
+		d.close()
+		if course == '1030':
+			for tag in d['python']:
+				self.assignments.append(Assignment(tag))
+		elif course == '1400':
+			for tag in d['java']:
+				self.assignments.append(Assignment(tag))
+		else:
+			print(f"Error - {self.period} not set as a programming class")
+			print(f"Check entry for {self.name}")
+			exit()
 
+	def clone(self, tag):
+		return f"nuames-cs/{tag}-{self.github}"
 	
-
-		
