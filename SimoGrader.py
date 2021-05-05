5fc968680d8c5c6b39fd500af04abb8397179b2e
mrsimonsen <johnsimonsen@weber.edu>
2021-05-05 15:21:29 -0600

    broke menu into submenus, working on adding grading to SimoGrader

--- SimoGrader.py
+++ SimoGrader.py
@@ -252,35 +252,76 @@ def main():
 	print("Welcome to the Simonsen AutoGrater Data Utility")
 	c = 14
 	while c != '0':
+		print("Main Menu")
 		print("0 - Quit")
-		print("1 - Reset Data")
-		print("2 - View Classes")
-		print("3 - Set Classes")
-		print("4 - Set All Students")
-		print("5 - View a Student's Assignments")
-		print("6 - Modify a Student")
-		print("7 - Modify a Student's Assignment")
-		print("8 - Delete old Repos")
+		print("1 - Grading Menu")
+		print("2 - Class Menu")
+		print("3 - Student Menu")
+		print("4 - Reset Database")
+		print("5 - Delete Old Repos")
 		c = input("What's your selection?\n")
-
-		if c =='0':
+		
+		if c == '0':
 			print("Goodbye")
-		elif c == '1':
-			reset_data()
-		elif c == '2':
-			display_classes()
-		elif c == '3':
-			set_periods()
 		elif c == '4':
-			set_students()
+			reset_data()
 		elif c == '5':
-			display_student()
-		elif c == '6':
-			mod_student()
-		elif c == '7':
-			mod_assign()
-		elif c == '8':
 			clean()
+		elif c == '1':
+			a = ''
+			while a != '0':
+				print("Grading Menu")
+				print("0 - Return to Main Menu")
+				print("1 - Grade an Assignment")
+				print("2 - Grade a Single Assignment")
+				print("3 - Generate Report")
+				a = input("What's your selection?\n")
+
+				if a=='0':
+					print("Returning to Main Menu")
+				if a == '
+				else:
+					print("That's not a valid menu option.")
+		elif c == '2':
+			a = ''
+			while a != '0':
+				print("Class Menu")
+				print("0 - Return to Main Menu")
+				print("1 - View Classes")
+				print("2 - Set Classes")
+				a = input("What's your selection?\n")
+
+				if a == '0':
+					print("Returning to Main Menu")
+				elif a == '1':
+					display_classes()
+				elif a == '2':
+					set_periods()
+				else:
+					print("That's not a valid menu option.")
+		elif c == '3':
+			a = ''
+			while a != '0':
+				print("Student Menu")
+				print("0 - Return to Main Menu")
+				print("1 - View a Student")
+				print("2 - Import Students")
+				print("3 - Modify a Student")
+				print("4 - Modify a Student's Assignment")
+				a = input ("What's your selection?\n")
+
+				if a == '0':
+					print("Returning to Main Menu")
+				elif a == '1':
+					display_student()
+				elif a == '2':
+					set_students()
+				elif a == '3':
+					mod_student()
+				elif a == '4':
+					mod_assign()
+				else:
+					print("That's not a valid menu option.")
 		else:
 			print("That's not a valid menu option.")
 
--- all.sh
+++
@@ -1,5 +0,0 @@
-python3 simogit.py
-cd Repos
-python3 grader.py
-cd ..
-rm -rf Repos assignment.txt
