e6db06cfdb64ddb266561a2b7a27663b37df09eb
mrsimonsen <johnsimonsen@weber.edu>
2021-05-12 10:48:34 -0600

    moved methods to make_data:

--- make_data.py
+++ make_data.py
@@ -1,7 +1,61 @@
-import shelve, csv, os
+import shelve, csv, os, datetime
 from student import Student
 from assignment import Assignment
 from subprocess import run
+from github import Github
+from dotenv import load_dotenv
+from alive_progress import alive_bar
+from sys import exit
+
+def get_date():
+	ok = False
+	while not ok:
+		year = validate_num("Year:(####)")
+		month = validate_num("Month:(##)")
+		day = validate("Day:(##)")
+		try:
+			d = datetime.datetime(year, month, day)
+			ok = True
+		except ValueError as e:
+			print(e)
+	return d
+
+def not_template(name):
+	if name[-3:] != ".py" and name[-5:] != ".java":
+		return True
+	return False
+
+def clean():
+	#get user credentials from .env
+	load_dotenv()
+	if (token := env.get('TOKEN')) == None:
+		exit("Edit .env file to have your personal access token.")
+	#make github object
+	g = Github(token)
+	print(f"Loaded credentials for {g.get_user().name}")
+	#makedir for clone - set name to current date time
+	repos = g.get_user().get_repos()
+	print("Gathering all repos older than what date?")
+	date = get_date()
+	print("Gathering Repos...")
+	old = []
+	total = len(list(repos))
+	print("Starting Search..")
+	with alive_bar(total, bar='classic', spinner='classic') as bar:
+		for r in repos:
+			if r.organization == "NUAMES-CS" and not_template(r.name) and r.updated_at < date:
+				old.append(r)
+			bar()
+	for i in old:
+		print(i)
+	print(f"{len(old)} repos collected")
+	if a := (input("Delete?\n").lower() in ('yes','y')):
+		for i in old:
+			print(f"deleting {i.name}")
+			i.delete()
+	if not a:
+		print("Nothing Deleted.")
+	print("Done.")
 
 def reset_data():
 	d = shelve.open('data.dat')
@@ -217,3 +271,102 @@ def grade(stu,tag):
 	else:
 		print(f"{stu.name} hasn't startd the assignment")
 
+def mod_student():
+	stu = select_student()
+	choice = -1
+	while choice != 0 and stu:
+		print(stu)
+		print("0 - Save/Back to Main Menu")
+		print("1 - Change name")
+		print("2 - Change github username")
+		print("3 - Change class period")
+		print("4 - Drop student")
+		choice = -1
+		while choice not in (0,1,2,3,4):
+			choice = validate_num("What would you like to change?")
+		if choice == 1:
+			print(f"Changing {stu.name} name:")
+			stu.name = change("Enter a new name:",stu.name)
+		elif choice == 2:
+			print(f"Changing {stu.name} username:")
+			stu.github = change("Enter a new github username:",stu.github)
+		elif choice == 3:
+			print(f"Changing {stu.name} period:")
+			stu.period = change("Enter a new period for username:",stu.period)
+		elif choice == 4:
+			print(f"Drop {stu.name}")
+			if ask_yn("Are you sure? This CANNOT be undone.") == 'y':
+				drop(stu)
+				choice = 0
+		elif choice == 0:
+			d = shelve.open('data.dat')
+			students = d['students']
+			for i in range(len(students)):
+				if students[i].name == stu.name:
+					students[i] = stu
+					break
+			d['students'] = students
+			d.close()
+			print('Data saved')
+
+def mod_assign():
+	stu = select_student()
+	choice = None
+	while choice != 0 and stu:
+		print(stu)
+		print(stu.print_assignments())
+		print("0 - Save/Back to Main Menu")
+		print("1 - Change a score")
+		print("2 - Set as late")
+		choice = -1
+		while choice not in (0,1,2):
+			choice = validate_num("What would you like to do?")
+		if choice == 1:
+			tag = validate_assign()
+			stu.assignments[tag].score = change_float("Enter a new score:", stu.assignments[tag].score)
+		elif choice == 2:
+			tag = validate_assign()
+			if ask_yn(f"Set {tag} to late?") == 'y':
+				stu.assignments[tag].set_late()
+		elif choice == 0:
+			d = shelve.open('data.dat')
+			students = d['students']
+			for i in range(len(students)):
+				if students[i].name == stu.name:
+					students[i] = stu
+					break
+			d['students'] = students
+			d.close()
+			print('Data saved')
+
+def grade_assignment():
+	tag = validate_assign()
+	d = shelve.open('data.dat')
+	students = d['students']
+	d.close()
+	if tag[-1] == 'j':
+		grading = '1400'
+	elif tag[-1] == 'p':
+		grading = '1030'
+	else:
+		grading = None
+	for stu in students:
+		if stu.course == grading:
+			a = stu.assignments[tag]
+			if a.score < 5 and a.late:
+				print(f"Grading {stu.name} - late")
+				grade(stu,tag)
+				print(f"{stu.name}: {tag} - {stu.assignments[tag].score}/10")
+			elif a.score < 10 and not a.late:
+				print(f"Cloning {stu.name} - on time")
+				grade(stu,tag)
+				print(f"{stu.name}: {tag} - {stu.assignments[tag].score}/10")
+			elif (a.score == 10 and not a.late) or (a.score == 5 and a.late):
+				print(f"{stu.name} already has completed assignment")
+			else:
+				print("something strange happened in SimoGrader.grade_assignment()")
+	print("Grading complete -- saving...")
+	d = shelve.open('data.dat')
+	d['students'] = students
+	d.close()
+	print("finished")
