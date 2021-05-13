e006a826fb3f86f9bec8eedd3738a503a21c80b9
mrsimonsen <johnsimonsen@weber.edu>
2021-05-13 10:50:40 -0600

    main menu broken up into functions

--- SimoGrader.py
+++ SimoGrader.py
@@ -1,6 +1,69 @@
 import shelve
 from make_data import *
 
+def grading_menu():
+	a = ''
+	while a != '0':
+		print("Grading Menu")
+		print("0 - Return to Main Menu")
+		print("1 - Grade an Assignment")
+		print("2 - Grade a single Student's Assignment")
+		print("3 - Generate Report")
+		a = input("What's your selection?\n")
+
+		if a=='0':
+			print("Returning to Main Menu")
+		elif a == '1':
+			grade_assignment()
+		elif a == '2':
+			grade_student()
+		elif a == '3':
+			report()
+		else:
+			print("That's not a valid menu option.")
+
+def class_menu():
+	a = ''
+	while a != '0':
+		print("Class Menu")
+		print("0 - Return to Main Menu")
+		print("1 - View Classes")
+		print("2 - Set Classes")
+		a = input("What's your selection?\n")
+
+		if a == '0':
+			print("Returning to Main Menu")
+		elif a == '1':
+			display_classes()
+		elif a == '2':
+			set_periods()
+		else:
+			print("That's not a valid menu option.")
+
+def student_menu():
+	a = ''
+	while a != '0':
+		print("Student Menu")
+		print("0 - Return to Main Menu")
+		print("1 - View a Student")
+		print("2 - Import Students")
+		print("3 - Modify a Student")
+		print("4 - Modify a Student's Assignment")
+		a = input ("What's your selection?\n")
+
+		if a == '0':
+			print("Returning to Main Menu")
+		elif a == '1':
+			display_student()
+		elif a == '2':
+			set_students()
+		elif a == '3':
+			mod_student()
+		elif a == '4':
+			mod_assign()
+		else:
+			print("That's not a valid menu option.")
+
 def main():
 	print("Welcome to the Simonsen AutoGrater Data Utility")
 	c = 14
@@ -16,70 +79,16 @@ def main():
 		
 		if c == '0':
 			print("Goodbye")
+		elif c == '1':
+			grading_men()
+		elif c == '2':
+			class_menu()
+		elif c == '3':
+			student_menu()
 		elif c == '4':
 			reset_data()
 		elif c == '5':
 			clean()
-		elif c == '1':
-			a = ''
-			while a != '0':
-				print("Grading Menu")
-				print("0 - Return to Main Menu")
-				print("1 - Grade an Assignment")
-				print("2 - Grade a single Student's Assignment")
-				print("3 - Generate Report")
-				a = input("What's your selection?\n")
-
-				if a=='0':
-					print("Returning to Main Menu")
-				elif a == '1':
-					grade_assignment()
-				elif a == '2':
-					grade_student()
-				elif a == '3':
-					report()
-				else:
-					print("That's not a valid menu option.")
-		elif c == '2':
-			a = ''
-			while a != '0':
-				print("Class Menu")
-				print("0 - Return to Main Menu")
-				print("1 - View Classes")
-				print("2 - Set Classes")
-				a = input("What's your selection?\n")
-
-				if a == '0':
-					print("Returning to Main Menu")
-				elif a == '1':
-					display_classes()
-				elif a == '2':
-					set_periods()
-				else:
-					print("That's not a valid menu option.")
-		elif c == '3':
-			a = ''
-			while a != '0':
-				print("Student Menu")
-				print("0 - Return to Main Menu")
-				print("1 - View a Student")
-				print("2 - Import Students")
-				print("3 - Modify a Student")
-				print("4 - Modify a Student's Assignment")
-				a = input ("What's your selection?\n")
-
-				if a == '0':
-					print("Returning to Main Menu")
-				elif a == '1':
-					display_student()
-				elif a == '2':
-					set_students()
-				elif a == '3':
-					mod_student()
-				elif a == '4':
-					mod_assign()
-				else:
-					print("That's not a valid menu option.")
 		else:
 			print("That's not a valid menu option.")
 
