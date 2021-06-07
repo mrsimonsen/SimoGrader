285abf9e3836b2a84fc8fe55fd2320dbd494cb9a
mrsimonsen <johnsimonsen@weber.edu>
2021-06-07 13:47:48 -0600

    20j complete

--- Testing/20j.java
+++ Testing/20j.java
@@ -1,73 +1,113 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.io.File;
+import java.io.PrintWriter;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Test20 {
-  static CaesarCipher student = new CaesarCipher();
-  static private ByteArrayOutputStream TOut;
-  static private ByteArrayInputStream TIn;
-  static private final PrintStream SOut = System.out;
-  static private final InputStream SIn = System.in;
+// 20j
+public class Tests {
+	static CaesarCipher student = new CaesarCipher();
+	private static ByteArrayOutputStream TOut;
+	private static ByteArrayInputStream TIn;
+	private static final PrintStream SOut = System.out;
+	private static final InputStream SIn = System.in;
+	private static int total = 0;
+	private static int passed = 0;
+	private static ArrayList<String> failed = new ArrayList<String>();
 
-  public static void main(String[] args){
-	  System.out.println(tests());    
-  }
+	public static void main(String[] args){
+		simple();
+		boolean verbose;
+		try{
+			verbose = !args[0].equals("simple");
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+			verbose = true;
+		}
+		if (verbose){
+			System.out.printf("Passed %d out of %d tests.\n",passed, total);
+			if (failed.size() > 0){
+				System.out.println("Failed:");
+				for (String i: failed){
+					System.out.printf("\t* %s\n",i);
+				}
+			}
+		}
+	}
 
-  public static String tests(){
-    int total = 0;
-    int score = 0;
-    //test1
-    total++;
-	setOutput();
-    String message = "Holy letters Batman, this is a string.";
-    int[] r = Support.letterDistr(message);
-    int[] c = {3,1,0,0,2,0,1,2,3,0,0,2,1,2,1,0,0,2,4,5,0,0,0,0,1,0};
+	public static void simple(){
+		test1();
+		test2();
+		test3();
+		test4();
+		System.out.printf("%d/%d\n",passed,total);
+	}
 
-    if (Arrays.equals(r,c)){
-      score++;
-    }
-    //test2
-    total++;
-    message = "This is my message.";
-    String result = Support.encrypt(message, 14);
-    String correct = "Hvwg wg am asggous.";
-    if (result.equals(correct)){
-      score++;
-    }
-    //test3
-    total++;
-    message = "Hvwg wg am asggous.";
-    result = Support.decrypt(message, 14);
-    correct = "This is my message.";
-    if (result.equals(correct)){
-      score++;
-    }
-    //test4
-    total++;
-    Scanner sinput = null;
-    String fname = "secret.txt";
-	String rout = "result";
-	String cout = "correct";
-	 try{
-		sinput = ReadWrite.open_file(fname,sinput);
-    	File file =	new File(fname);
-    	Scanner cinput	= new	Scanner(file);	 
-		rout = sinput.nextLine();
-    	cout = cinput.nextLine();
-	 }
-	  catch (FileNotFoundException e){
-		  System.out.println(e);
-	  }
+	public static void test1(){
+		total++;
+		int[] result = Support.letterDistr("Holy letters Batman, this is a string.");
+		int[] correct = {3,1,0,0,2,0,1,2,3,0,0,2,1,2,1,0,0,2,4,5,0,0,0,0,1,0};
+		if (Arrays.equals(result,correct)){
+			score++;
+		}
+		else{
+			failed.add("test1");
+		}
+	}
 
+	public static void test2(){
+		total++;
+		String result = Support.encrypt("This is my message.", 14);
+		String correct = "Hvwg wg am asggous.";
+		if (result.equals(correct)){
+			score++;
+		}
+		else{
+			failed.add("test2");
+		}
+	}
 
-    if (rout.equals(cout)){
-      score++;
-    }
+	public static void test3(){
+		total++;
+		String result = Support.decrypt("Hvwg wg am asggous.", 14);
+		String correct = "This is my message.";
+		if (result.equals(correct)){
+			score++;
+		}
+		else{
+			failed.add("test3");
+		}
+	}
+
+	public static void test4(){
+		total++;
+		Scanner sinput = null;
+		String fname = "secret.txt";
+		String rout = "result";
+		String cout = "correct";
+		try{
+			sinput = ReadWrite.open_file(fname,sinput);
+			Scanner cinput = new Scanner(new File(fname));
+			rout = sinput.nextLine();
+			cout = cinput.nextLine();
+			sinput.close();
+			cinput.close();
+		}
+		catch (FileNotFoundException e){
+			System.out.println(e);
+		}
+		if (rout.equals(cout)){
+			score++;
+		}
+		else{
+			failed.add("test4");
+		}
+	}
 /*
     //hidden test
     total++;
@@ -82,27 +122,45 @@ public class Test20 {
     if (result.equals(correct)){
       score++;
     }
+*/
 
-  */  //testing complete
-  	restoreSystem();
-    String rep = ""+ score +"/"+total;
-    return rep;
-  }
-
-  //Set up methods
-  static public void setOutput(){
-    TOut = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(TOut));
-  }
-  private static void setInput(String data){
-  TIn = new ByteArrayInputStream(data.getBytes());
-  System.setIn(TIn);
-  }
-  static private String getOutput(){
-    return TOut.toString();
-  }
-  static public void restoreSystem(){
-    System.setOut(SOut);
-    System.setIn(SIn);
-  }
+	//Set up methods
+	 public static void setOutput(){
+	 	TOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(TOut));
+	}
+	private static void setInput(String data){
+		TIn = new ByteArrayInputStream(data.getBytes());
+		System.setIn(TIn);
+	}
+	private static String getOutput(String input){
+		setOutput();
+		setInput(input);
+		student.main(null);
+		String result = TOut.toString();
+		restoreSystem();
+		return result;
+	}
+	public static void restoreSystem(){
+		System.setOut(SOut);
+		System.setIn(SIn);
+	}
+	public static void toFile(String correct, String result){
+		try{
+			File f;
+			PrintWriter p;
+			String[] a = {"correct","result"};
+			String[] args = {correct,result};
+			for (int i = 0; i<2;i++){
+				f = new File(a[i]+".txt");
+				p = new PrintWriter(f);
+				p.print(args[i]);
+				p.close();
+			}
+		}
+		catch (FileNotFoundException e){
+			System.out.println("Couldn't create files.");
+			System.out.println(e);
+		}
+	}
 }
