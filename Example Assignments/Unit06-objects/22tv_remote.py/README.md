fd24d722f3283ef3802767d5dc430547f0958eb7
mrsimonsen <johnsimonsen@weber.edu>
2022-09-19 14:49:35 -0600

    Final lessons and projects restored

---
+++ Example Assignments/Unit06-objects/22tv_remote.py/README.md
@@ -0,0 +1,80 @@
+# Instructions
+Objective: Learn about private attributes, getters and setters.
+
+## Steps
+1. You've been given the main() method for this assignment. Do not edit it.
+2. Create the Remote class with a constructor that sets default values for the following private attributes:
+	attribute|value
+	--|--
+	channel|3
+	volume|5
+3. Create a string method that returns the attributes of the object like this example:
+```
+Channel: 3
+Volume: 5
+```
+4. Create a public volume_up() method that increases the private volume attribute by 1. Have the max volume be 10.
+5. Create a public volume_down() method that decreases the private volume attribute by 1. Have the min volume be 0.
+6. Create a public channel_up method that increases the private channel attribute by 1. Have the channel loop with a max channel of 100. As in channel_up 100 = 1
+7. Create a public channel_down method that decreases the private channel attribute by 1. Have the channel loop with a min channel of 1. As in channel_down 1 = 100.
+8. Create a getter for the private channel attribute.
+9. Create a setter for the private channel attribute. The setter follows this psuedocode:
+```
+try to convert the new value to an int
+if that fails display "Error: {e}", where {e} is the caught error message, and "Explanation: '{new}' isn't a number", where {new} is the new value
+check if the converted new value is within the channel range
+if it is, set the private channel attribute to the new value
+otherwise display "'{new}' is out of the channel range"
+```
+
+The output of the program should look like the following:
+```
+Channel: 3
+Volume: 5
+vu - Volume Up
+vd - Volume Down
+cu - Channel Up
+cd - Channel Down
+set - Set Channel
+q - Quit
+Select an option:
+vu
+vu - Volume Up
+vd - Volume Down
+cu - Channel Up
+cd - Channel Down
+set - Set Channel
+q - Quit
+Select an option:
+vu
+vu - Volume Up
+vd - Volume Down
+cu - Channel Up
+cd - Channel Down
+set - Set Channel
+q - Quit
+Select an option:
+set
+What channel?
+14
+vu - Volume Up
+vd - Volume Down
+cu - Channel Up
+cd - Channel Down
+set - Set Channel
+q - Quit
+Select an option:
+v
+Channel: 14
+Volume: 7
+vu - Volume Up
+vd - Volume Down
+cu - Channel Up
+cd - Channel Down
+set - Set Channel
+q - Quit
+Select an option:
+q
+Goodbye.
+```
+*Note: This program outputs a newline after each input and the example show the user's input, but that actually isn't part of the output when the program runs.*
---
+++ Example Assignments/Unit06-objects/22tv_remote.py/tests.py
@@ -0,0 +1,154 @@
+import unittest, sys
+from unittest.mock import patch
+from io import StringIO
+import student
+
+class Tests(unittest.TestCase):
+	def test01_part_2(self):
+		correct = (3,5)
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			channel = r._Remote__channel
+		except AttributeError:
+			self.fail("Message: channel attribute is not private")
+		try:
+			volume = r._Remote__volume
+		except AttributeError:
+			self.fail("Message: volume attribute is not private")
+		result = (channel, volume)
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test02_part_3(self, stdout):
+		correct = "Channel: 3\nVolume: 5\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		print(r)
+		result = stdout.getvalue()
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test03_part_4(self, stdout):
+		correct = "Channel: 3\nVolume: 10\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			for i in range(7):
+				r.volume_up()
+		except AttributeError:
+			self.fail("Message: volume_up() method has not been defined")
+		print(r)
+		result = stdout.getvalue()
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test04_part_5(self, stdout):
+		correct = "Channel: 3\nVolume: 0\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			for i in range(7):
+				r.volume_down()
+		except AttributeError:
+			self.fail("Message: volume_down() method has not been defined")
+		print(r)
+		result = stdout.getvalue()
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test05_part_6(self, stdout):
+		correct = "Channel: 1\nVolume: 5\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			for i in range(98):
+				r.channel_up()
+		except AttributeError:
+			self.fail("Message: channel_up() method has not been defined")
+		print(r)
+		result = stdout.getvalue()
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test06_part_7(self, stdout):
+		correct = "Channel: 100\nVolume: 5\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			for i in range(3):
+				r.channel_down()
+		except AttributeError:
+			self.fail("Message: channel_down() method has not been defined")
+		print(r)
+		result = stdout.getvalue()
+		self.assertEqual(result, correct)
+
+	def test07_part_8(self):
+		correct = 3
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			result = r.channel
+		except AttributeError:
+			self.fail("Message: channel attribute does not have a getter method")
+		self.assertEqual(result, correct)
+
+	def test08_part_9(self):
+		correct = 14
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			r.channel = 14
+			result = r.channel
+		except AttributeError:
+			self.fail("Message: channel attribute does not have a setter method")
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test09_part_9(self, stdout):
+		correct = "Error: invalid literal for int() with base 10: 'test'\nExplanation: 'test' isn't a number\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			r.channel = 'test'
+			result = stdout.getvalue()
+		except AttributeError:
+			self.fail("Message: channel attribute does not have a setter method")
+		self.assertEqual(result, correct)
+
+	@patch('sys.stdout', new_callable = StringIO)
+	def test10_part_9(self, stdout):
+		correct = "'3000' is out of the channel range\n"
+		try:
+			r = student.Remote()
+		except AttributeError:
+			self.fail("Message: Remote class has not been defined.")
+		try:
+			r.channel = 3000
+			result = stdout.getvalue()
+		except AttributeError:
+			self.fail("Message: channel attribute does not have a setter method")
+		self.assertEqual(result, correct)
+
+
+if __name__ == "__main__":
+	unittest.main(module='tests', failfast=True)
