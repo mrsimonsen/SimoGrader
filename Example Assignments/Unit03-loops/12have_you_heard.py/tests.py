c52240d5d377f43988bf8308af777631b2223847
mrsimonsen <johnsimonsen@weber.edu>
2022-09-19 14:10:05 -0600

    Lesson12 restored

--- Example Assignments/Unit03-loops/12have_you_heard.py/tests.py
+++ Example Assignments/Unit03-loops/12have_you_heard.py/tests.py
@@ -32,31 +32,6 @@ class Tests(unittest.TestCase):
 		result = stdout.getvalue()
 		self.assertEqual(result, correct)
 
-def main(simple):
-	suite = unittest.defaultTestLoader
-	runner = unittest.TextTestRunner(stream=StringIO(), descriptions=False)
-	result = runner.run(suite.loadTestsFromTestCase(Tests))
-	total = result.testsRun
-	if result.wasSuccessful():
-		score = 10
-		passed = total
-	else:
-		passed = total - len(result.failures) - len(result.errors)
-		score = round(passed/total*10,2)
-	report = f"Passed: {passed}/{total}\nScore: {score}\n"
-	if not simple:
-		failed = []
-		for i in result.failures:
-			failed.append(f"Fail: {i[0].id()[15:]}")
-		for i in result.errors:
-			failed.append(f"Error: {i[0].id()[15:]}")
-		report += "Failed:\n"
-		for i in failed:
-			report += f"\t{i}\n"
-	return score, report
 
-if __name__ == '__main__':
-	score, report = main(sys.argv[1]=='True')
-	with open('score.txt','w') as f:
-		f.write(str(score))
-		f.write('\n'+report)
+if __name__ == "__main__":
+	unittest.main(module='tests', failfast=True)
