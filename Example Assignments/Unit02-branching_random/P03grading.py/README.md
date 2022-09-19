de64fc6cc5fad16bd046e5a6ab690276d9a33934
mrsimonsen <johnsimonsen@weber.edu>
2022-09-19 14:01:37 -0600

    Project03 restored

---
+++ Example Assignments/Unit02-branching_random/P03grading.py/README.md
@@ -0,0 +1,36 @@
+# Instructions
+Project 3
+
+## Steps
+1. Display a greeting to the user and display the provided grade categories and percentages. Each category starts with a tab character and the head is left justified to 10 characters. Be sure to use the variables for the percentage because they will change in the tests.
+2. Prompt the user for the percentage earned from each category as a whole number.
+3. Calculate the overall grade and assign a letter based on the following table. Display the final grade.
+
+Letter | Percentage
+ :--: | :--:
+F | <60%
+D | >=60% <70%
+C | >=70% <80%
+B | >=80% <90%
+A | >=90%
+
+The output of the program should look like the following:
+```
+----Grade Calculator----
+I'll calculate a student's final grade based on the following categories:
+Grade Categories:
+	Homework: 50%
+	Quizzes:  20%
+	Midterm:  10%
+	Final:    20%
+Enter percent earned from Homework:
+95
+Enter percent earned from Quizzes:
+60
+Enter percent earned from Midterm:
+88
+Enter percent earned from Final:
+85
+Final Grade: B (85.3%)
+```
+*Note: This program outputs a newline after each input and the example show the user's input, but that actually isn't part of the output when the program runs.*
---
+++ Example Assignments/Unit02-branching_random/P03grading.py/tests.py
@@ -0,0 +1,81 @@
+import unittest, sys, random
+from unittest.mock import patch
+from io import StringIO
+import student
+
+class Tests(unittest.TestCase):
+	inputs = "0\n0\n0\n0\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test01_part_1_default(self, stdout):
+		correct = "----Grade Calculator----\nI'll calculate a student's final grade based on the following categories:\nGrade Categories:\n\tHomework: 50%\n\tQuizzes:  20%\n\tMidterm:  10%\n\tFinal:    20%\n\n"
+		student.main()
+		result = stdout.getvalue()[:len(correct)]
+		self.assertEqual(result, correct)
+
+	inputs = "0\n0\n0\n0\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test02_part_1_extra(self, stdout):
+		correct = "----Grade Calculator----\nI'll calculate a student's final grade based on the following categories:\nGrade Categories:\n\tHomework: 10%\n\tQuizzes:  20%\n\tMidterm:  30%\n\tFinal:    40%\n\n"
+		student.main(10,20,30,40)
+		result = stdout.getvalue()[:len(correct)]
+		self.assertEqual(result, correct)
+
+	inputs = "95\n60\n88\n85\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test03_part_2_default(self, stdout):
+		correct = "Enter percent earned from Homework:\nEnter percent earned from Quizzes:\nEnter percent earned from Midterm:\nEnter percent earned from Final:\n"
+		student.main()
+		result = stdout.getvalue()[178:len(correct)+178]
+		self.assertEqual(result, correct)
+
+	inputs = "95\n73\n89\n95\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test04_part_3_A(self, stdout):
+		correct = "\nFinal Grade: A (90.0%)\n"
+		student.main()
+		result = stdout.getvalue()[317:]
+		self.assertEqual(result, correct)
+
+	inputs = "85\n70\n82\n95\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test05_part_3_B(self, stdout):
+		correct = "\nFinal Grade: B (85.1%)\n"
+		student.main(10,20,30,40)
+		result = stdout.getvalue()[317:]
+		self.assertEqual(result, correct)
+
+	inputs = "65\n70\n80\n95\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test06_part_3_C(self, stdout):
+		correct = "\nFinal Grade: C (73.5%)\n"
+		student.main(40,20,30,10)
+		result = stdout.getvalue()[317:]
+		self.assertEqual(result, correct)
+
+	inputs = "65\n70\n80\n65\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test07_part_3_D(self, stdout):
+		correct = "\nFinal Grade: D (67.0%)\n"
+		student.main(10,10,10,70)
+		result = stdout.getvalue()[317:]
+		self.assertEqual(result, correct)
+
+	inputs = "5\n30\n80\n65\n"
+	@patch('sys.stdin', StringIO(inputs))
+	@patch('sys.stdout', new_callable = StringIO)
+	def test08_part_3_F(self, stdout):
+		correct = "\nFinal Grade: F (35.75%)\n"
+		student.main(10,80,10,5)
+		result = stdout.getvalue()[316:]
+		self.assertEqual(result, correct)
+
+
+if __name__ == "__main__":
+	unittest.main(module='tests', failfast=True)
