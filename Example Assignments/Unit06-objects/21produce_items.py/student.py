5149669074c7640f3b1280dc3ca648cab4813f48
mrsimonsen <johnsimonsen@weber.edu>
2023-03-22 09:55:36 -0600

    added a main interface for manual testing

--- Example Assignments/Unit06-objects/21produce_items.py/student.py
+++ Example Assignments/Unit06-objects/21produce_items.py/student.py
@@ -1,1 +1,26 @@
+def main():
+	item1 = Item()
+	item2 = Produce()
+
+	print("Item 1")
+	name = input("Enter the item name:\n")
+	price = float(input("Enter the item price:\n"))
+	quan = int(input("Enter the item quantity:\n"))
+	item1.name = name
+	item1.price = price
+	item1.quantity = quan
+
+	print("\nItem 2 - Produce")
+	item2.name = input("Enter the item name:\n")
+	item2.price = float(input("Enter the item price:\n"))
+	item2.quantity = int(input("Enter the item quantity:\n"))
+	item2.expire = input("When does the item expire:\n")
+
+	print("\nTOTAL COST")
+	print(item1)
+	print(item2)
+	print(f"\nTotal: ${item1.total() + item2.total():.2f}")
+################################################
+#Do not change code above this line
+
 #copy assignment 20
